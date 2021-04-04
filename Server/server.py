5fedbcd8d10065b3e6faffae13375559f2df2b4f
Sonal778 <62555666+Sonal778@users.noreply.github.com>
2021-04-04 22:13:58 +0530

    Update server.py

--- Server/server.py
+++ Server/server.py
@@ -1,14 +1,10 @@
 import random
 
-import uvicorn
-from fastapi import FastAPI, Request
-
+from flask import Flask, request
 from transformers import T5ForConditionalGeneration, T5Tokenizer
 import torch
 
-import tensorflow_hub as hub
-
-app = FastAPI()
+app = Flask(__name__)
 
 output_cache = []
 input_sentence = ""
@@ -17,7 +13,7 @@ input_sentence = ""
 # Selecting the tokenizer
 def select_tokenizer(tokenizer_name):
     if tokenizer_name == "t5-small":
-        tokenizer = T5Tokenizer.from_pretrained('t5-small')
+        tokenizer = T5Tokenizer.from_pretrained('T5-small')
     else:
         tokenizer = T5Tokenizer.from_pretrained('Vamsi/T5_Paraphrase_Paws')
     return tokenizer
@@ -29,34 +25,17 @@ def run_model(sentence, decoding_params, tokenizer, model):
 
     text = "paraphrase: " + sentence + " </s>"
 
-    max_len = decoding_params['max_len']
+    max_len = decoding_params["max_len"]
 
     encoding = tokenizer.encode_plus(text, pad_to_max_length=True, return_tensors="pt")
     input_ids, attention_masks = encoding["input_ids"].to(device), encoding["attention_mask"].to(device)
 
-    if decoding_params['strategy'] == "Greedy Decoding":
-        beam_outputs = model.generate(
-            input_ids=input_ids, attention_mask=attention_masks,
-            max_length=max_len,
-            # num_return_sequences=decoding_params["return_sen_num"]
-        )
-    elif decoding_params['strategy'] == "Beam Search":
-        beam_outputs = model.generate(
-            input_ids=input_ids, attention_mask=attention_masks,
-            max_length=max_len,
-            num_beams=decoding_params["beams"],
-            no_repeat_ngram_size=decoding_params['ngram'],
-            early_stopping=True,
-            temperature=decoding_params['temperature'],
-            num_return_sequences=decoding_params['return_sen_num']  # Number of sentences to return
-        )
-    else:
-        beam_outputs = model.generate(
+    beam_outputs = model.generate(
             input_ids=input_ids, attention_mask=attention_masks,
             do_sample=True,
             max_length=max_len,
-            top_k=decoding_params['top_k'],
-            top_p=decoding_params['top_p'],
+            top_k=decoding_params["top_k"],
+            top_p=decoding_params["top_p"],
             early_stopping=True,
             # temperature=decoding_params["temperature"],
             num_return_sequences=decoding_params["return_sen_num"]  # Number of sentences to return
@@ -91,38 +70,21 @@ def checkDuplicate(paraphrase, decoding_params, temp):
 def preprocess_output(model_output, tokenizer, temp, sentence, decoding_params, model):
     for line in model_output:
         paraphrase = tokenizer.decode(line, skip_special_tokens=True, clean_up_tokenization_spaces=True)
-        if paraphrase.lower() != sentence.lower() and paraphrase not in temp:
-            if decoding_params['strategy'] == "Top-k, Top-p sampling":
-                if checkDuplicate(paraphrase, decoding_params, temp):
-                    temp.append(paraphrase)
-            else:
-                temp.append(paraphrase)
-
-    if decoding_params['strategy'] != "Greedy Decoding" and len(temp) < decoding_params['return_sen_num']:
-        temp1 = temp
-        if decoding_params['strategy'] == "Top-k, Top-p sampling":
-            sentence = input_sentence
-        else:
-            sentence = temp1[random.randint(0, len(temp1)-1)]
-
-        model_output = run_model(sentence, decoding_params, tokenizer, model)
-        temp = preprocess_output(model_output, tokenizer, temp, sentence, decoding_params, model)
-    return temp
+    return paraphrase
 
 
-@app.post("/run_forward")
-async def forward(request: Request):
-    params = await request.json()
-    sentence = params['sentence']
-    print(sentence)
-    decoding_params = params['decoding_params']
+@app.route("/run", methods=["POST"])
+def forward():
+    params = request.get_json()
+    sentence = params["sentence"]
+    decoding_params = params["decoding_params"]
 
     global input_sentence
-    input_sentence = str(sentence)
+    input_sentence = sentence
 
-    tokenizer_name = decoding_params['tokenizer']
-    model = T5ForConditionalGeneration.from_pretrained('Vamsi/T5_Paraphrase_Paws')
-    tokenizer = select_tokenizer(tokenizer_name)
+    tokenizer_name = decoding_params["tokenizer"]
+    model = T5ForConditionalGeneration.from_pretrained('/Server/Model/')
+    tokenizer = select_tokenizer('/Server/Token/')
 
     model_output = run_model(sentence, decoding_params, tokenizer, model)
 
@@ -131,50 +93,26 @@ async def forward(request: Request):
 
     temp = preprocess_output(model_output, tokenizer, temp, sentence, decoding_params, model)
 
-    global output_cache
-    output_cache = temp
+    return {"data": temp}
 
-    for i, line in enumerate(temp):
-        paraphrases.append(f"{i + 1}. {line}")
 
-    print({"data": paraphrases})
-    return {"data": paraphrases}
+@app.route("/embedding", methods=["POST"])
+def embedding():
+    params = request.get_json()
 
-@app.get('/run')
-def runner():
-    # if key doesn't exist, returns None
-    sentence = request.args.get('sentence')
-    print(sentence)
-    param1 = request.args.get('decoding_params')
-    decoding_params = param1.get_json()
+    sentence = params["sentence"]
+    paraphrased_sentences = output_cache
 
-    global input_sentence
-    input_sentence = str(sentence)
+    paraphrased_sentences.append(sentence)
 
-    tokenizer_name = decoding_params['tokenizer']
-    model = T5ForConditionalGeneration.from_pretrained('Vamsi/T5_Paraphrase_Paws')
-    tokenizer = select_tokenizer(tokenizer_name)
+    module_url = "https://tfhub.dev/google/universal-sentence-encoder/4"
+    model_USE = hub.load(module_url)
 
-    model_output = run_model(sentence, decoding_params, tokenizer, model)
+    embedding_vectors = model_USE(paraphrased_sentences)
+    # print(embedding_vectors.numpy().tolist())
 
-    paraphrases = []
-    temp = []
-
-    temp = preprocess_output(model_output, tokenizer, temp, sentence, decoding_params, model)
-
-    global output_cache
-    output_cache = temp
-
-    for i, line in enumerate(temp):
-        paraphrases.append(f"{i + 1}. {line}")
-
-    print({"data": paraphrases})
-    return {"data": paraphrases}
-
-@app.post("/hello")
-def helo():
-    return {"data": "Hello World"}
+    return {"data": embedding_vectors.numpy().tolist(), "paraphrased": paraphrased_sentences}
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(debug = False, port = 8080, host = '0.0.0.0', threaded = True)
