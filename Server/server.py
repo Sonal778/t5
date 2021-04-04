d8914b769c361824ec06bf19a39928a2e9739f7b
Sonal778 <62555666+Sonal778@users.noreply.github.com>
2021-04-04 22:18:23 +0530

    Update server.py

--- Server/server.py
+++ Server/server.py
@@ -115,4 +115,4 @@ def embedding():
 
 
 if __name__ == "__main__":
-    app.run(debug = False, port = 8080, host = '0.0.0.0', threaded = True)
+    app.run(debug = False, port = 8000, host = '0.0.0.0', threaded = True)
